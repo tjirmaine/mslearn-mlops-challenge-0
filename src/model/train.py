b1faf288884a0391972471435cba7da8937c9a43
jirmaine <jirmaine@typetone.ai>
2025-08-01 21:49:16 +0200

    Add data splitting functionality and enable MLflow autologging in training script

--- src/model/train.py
+++ src/model/train.py
@@ -5,14 +5,15 @@ import glob
 import os
 
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import mlflow
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -36,9 +37,32 @@ def get_csvs_df(path):
 # TO DO: add function to split data
 
 
+def split_data(df):
+    x_columns = [
+        "Pregnancies",
+        "PlasmaGlucose",
+        "DiastolicBloodPressure",
+        "TricepsThickness",
+        "SerumInsulin",
+        "BMI",
+        "DiabetesPedigree",
+        "Age",
+    ]
+    y_column = "Diabetic"
+
+    X, y = (
+        df[x_columns].values,
+        df[y_column].values,
+    )
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
+    return X_train, X_test, y_train, y_test
+
+
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
@@ -46,16 +70,15 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    parser.add_argument("--training_data", dest="training_data", type=str)
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
